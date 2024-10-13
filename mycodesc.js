51185d77582b291adf318c816719c43f45d51787
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2024-10-13 11:50:07 -0400

    Add files via upload

---
+++ mycodesc.js
@@ -0,0 +1,119 @@
+mhs: Physarum polycephalum, literally the "many-headed slime", is a slime mold that inhabits shady, cool, moist areas, such as decaying leaves and logs.
+
+bsl: Cladonia cristatella, commonly known as the British soldiers lichen or the British soldiers cup lichen, is a fruticose, cup lichen belonging to the family Cladoniaceae. The species was first described scientifically by American botanist Edward Tuckerman in 1858.
+
+waspmold: Metatrichia vesparium. Slime molds or Myxomycetes are similar to fungi but in a category of their own. The fluffy little fibers are coiled up inside and then burst out to help with spore dispersal. Whats left is the little wasp nest looking structure.
+
+ppc: Cladonia pyxidata or the pebbled cup lichen is a species of cup lichen in the family Cladoniaceae. It is host to the lichenicolous fungus Lichenoconium pyxidatae.
+
+ls: Cladonia deformis, also known as the lesser sulphur cup or the lesser sulphur cup lichen,[1] is a light-coloured, fruticose, cup lichen belonging to the family Cladoniaceae.[2] This lichen was first described as Lichen deformis by Carl Linnaeus in 1753.
+
+greylegs: Physcia pumilior
+
+rfpc: Cladonia pleurota is a species of fruticose, cup lichen in the family Cladoniaceae. It was first formally described as a new species by German lichenologist Heinrich Gustav Flörke in 1808 as Capitularia pleurota.[1] Ludwig Emanuel Schaerer transferred it to the genus Cladonia in 1850.[2] In North America, it is known colloquially as the red-fruited pixie cup[3] or the red-fruited cup lichen.
+
+thorn: Cladonia uncialis is a species of fruticose, cup lichen in the family Cladoniaceae. It was first described as a new species by Swedish taxonomist Carl Linnaeus in 1753. It was transferred to the genus Cladonia by Friedrich Heinrich Wiggers in 1780. In North America, the lichen is colloquially known as the thorn Cladonia or the thorn cup lichen.
+
+sd: Chrysothrix chlorina, is a type of lichen that is commonly found in northern boreal regions around the world. It is a member of the Chrysotrichaceae family and is characterized by its bright yellow to greenish-yellow color and powdery texture.
+
+ladder: Cladonia verticillata Basal squamules entire or with shallow, rounded lobes, white below, upper surfaces bluish-grey when fresh. Podetia with regular scyphi, +|- abruptly tapered to stalk with 2-5 proliferating tiers arising from centre of scyphus. 
+
+pretzel: Hemitrichia serpula When mature, occurring as pretzel-like network of bright yellow to mustard yellow veins or strands. The outer walls break down, revealing a cottony, mustard-yellow spore mass.  
+
+hc: Ceratiomyxa fruticulosa The tiny, erect, branched or simple structure of this organism look like small icicles or like tiny pieces of
+erect coral. They are whitish and translucent with a fuzzy appearance because they produce their spores on their outside surfaces.       
+
+swarmingspine: Mucronella calva saprobic on dead, fallen, decayed logs.
+
+turban: Cladonia peziziformis or the turban cup lichen is a species of cup lichen in the family Cladoniaceae.  
+
+pel: Dibaeis baeomyces, commonly known as pink earth lichen, is a fruticose lichen belonging to the family Icmadophilaceae.
+
+lp: Cladonia macilenta An open, disk-shaped or cup-shaped, reproductive structure, with spore sacs on the upper surface, that produces spores for the fungal partner of a lichen.
+
+vfl: Stereocaulon vesuvianum is a species of snow lichen belonging to the family Stereocaulaceae.
+
+tsol: Cladonia bellidiflora Small thallus made up of pale, yellow-green and frilly lobes. Contains small, erect, branch- like stems ending in cup-shaped tips rimmed with bright red apothecia. On rotting stumps and logs, bare soil, and among mosses in the open and in all forested habitats.
+
+branchedpc: Cladonia ramulosa A widespread, rather scruffy Cladonia, found in a wide range of acid to neutral habitats, on soil, humus decaying vegetation and wood. Easily confused with other species but a combination of characters picks it out.
+
+odisco: Aleurodiscus amorphus a type of lichen with an orange disc-shaped spore-bearing structure, or apothecium, on its surface. The apothecia rim is the same color as the rest of the lichen, while the center is darker orange.
+
+firedot: Caloplaca flavovirescens
+
+lung: Lobaria pulmonaria is a large epiphytic lichen consisting of an ascomycete fungus and a green algal partner living together in a symbiotic relationship with a cyanobacterium—a symbiosis involving members of three kingdoms of organisms.
+
+dragonhorn: Cladonia squamosa The primary thallus is composed of medium-sized, crenate squamules, approximately 1.5-7mm. long, and 1-5mm. wide. The upper surface is fawn or tan to cinnamon-colored varying toward greenish grey. The apothecia are small, ranging from .5–3 mm. in diameter, and are located on the margin of the cups or at the ends of branches or proliferations. They are fawn to cinnamon-colored. The paraphyses are usually simple, sometimes thickened, and are brownish towards the apex. The hymenium is pale or pale-brownish below and brownish above. The asci are lecanoralean, with a thickened tholus. There are a usually 8 ascospores, which are oblong or oblong-obtuse to fusiform in shape, between 5-17 μm. long and 2.5-3.5 μm. wide. Conidia are falcate and 3-8 μm. long.
+
+reindeer: Cladonia rangiferina, also known as reindeer cup lichen, reindeer lichen (cf. Sw. renlav) or grey reindeer lichen, is a light-coloured fruticose, cup lichen species in the family Cladoniaceae. It grows in both hot and cold climates in well-drained, open environments. Found primarily in areas of alpine tundra, it is extremely cold-hardy. 
+
+cylpow: Cladonia cylindrica Thallus dimorphic, composed of a squamulose primary thallus and fruticose secondary thallus consisting of podetia. Squamules persistent, sorediate or not, dentate to lobed, usually with sorediate margins; ~1.5–3 mm wide by 1.5–3 mm long; upper surface blue-gray, smooth, corticate; lower surface white, ecorticate. Podetia simple, tapering, without cups, ecorticate, sorediate, covered in coarse to fine soredia; ~3–5 mm tall by 0.5–1 mm thick. Ascomata biatorine apothecia, brown, usually inconspicuous; pycnidia brown, usually inconspicuous.
+
+
+
+
+
+enada:
+
+cl: 
+
+so:
+
+ge:
+
+chrom:
+
+ahm:
+
+reddock:
+
+dryad:
+
+lb:
+
+vpspore:
+
+hoyster:
+
+vermwaxcap:
+
+tenuipes:
+
+wcoral:
+
+cbf;
+
+cparchment:
+
+mycena;
+
+greenwood:
+
+yfc;
+
+panellus;
+
+gfmould:
+
+mpball:
+
+et:
+
+ttail:
+
+tubelet:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
