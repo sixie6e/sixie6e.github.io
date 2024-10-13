25c5810fd799e1d95d60e2a9af6788754be1c646
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2024-10-13 15:50:45 -0400

    Update mycodesc.js

--- mycodesc.js
+++ mycodesc.js
@@ -6,17 +6,17 @@ waspmold: Metatrichia vesparium. Slime molds or Myxomycetes are similar to fungi
 
 ppc: Cladonia pyxidata or the pebbled cup lichen is a species of cup lichen in the family Cladoniaceae. It is host to the lichenicolous fungus Lichenoconium pyxidatae.
 
-ls: Cladonia deformis, also known as the lesser sulphur cup or the lesser sulphur cup lichen,[1] is a light-coloured, fruticose, cup lichen belonging to the family Cladoniaceae.[2] This lichen was first described as Lichen deformis by Carl Linnaeus in 1753.
+ls: Cladonia deformis, also known as the lesser sulphur cup or the lesser sulphur cup lichen, is a light-coloured, fruticose, cup lichen belonging to the family Cladoniaceae.[2] This lichen was first described as Lichen deformis by Carl Linnaeus in 1753.
 
 greylegs: Physcia pumilior
 
-rfpc: Cladonia pleurota is a species of fruticose, cup lichen in the family Cladoniaceae. It was first formally described as a new species by German lichenologist Heinrich Gustav Flörke in 1808 as Capitularia pleurota.[1] Ludwig Emanuel Schaerer transferred it to the genus Cladonia in 1850.[2] In North America, it is known colloquially as the red-fruited pixie cup[3] or the red-fruited cup lichen.
+rfpc: Cladonia pleurota is a species of fruticose, cup lichen in the family Cladoniaceae. It was first formally described as a new species by German lichenologist Heinrich Gustav Flörke in 1808 as Capitularia pleurota. Ludwig Emanuel Schaerer transferred it to the genus Cladonia in 1850. In North America, it is known colloquially as the red-fruited pixie cup or the red-fruited cup lichen.
 
 thorn: Cladonia uncialis is a species of fruticose, cup lichen in the family Cladoniaceae. It was first described as a new species by Swedish taxonomist Carl Linnaeus in 1753. It was transferred to the genus Cladonia by Friedrich Heinrich Wiggers in 1780. In North America, the lichen is colloquially known as the thorn Cladonia or the thorn cup lichen.
 
 sd: Chrysothrix chlorina, is a type of lichen that is commonly found in northern boreal regions around the world. It is a member of the Chrysotrichaceae family and is characterized by its bright yellow to greenish-yellow color and powdery texture.
 
-ladder: Cladonia verticillata Basal squamules entire or with shallow, rounded lobes, white below, upper surfaces bluish-grey when fresh. Podetia with regular scyphi, +|- abruptly tapered to stalk with 2-5 proliferating tiers arising from centre of scyphus. 
+ladder: Cladonia verticillata Basal squamules entire or with shallow, rounded lobes, white below, upper surfaces bluish-grey when fresh. Podetia with regular scyphi, 2-5 proliferating tiers arising from centre of scyphus. 
 
 pretzel: Hemitrichia serpula When mature, occurring as pretzel-like network of bright yellow to mustard yellow veins or strands. The outer walls break down, revealing a cottony, mustard-yellow spore mass.  
 
@@ -43,77 +43,58 @@ firedot: Caloplaca flavovirescens
 
 lung: Lobaria pulmonaria is a large epiphytic lichen consisting of an ascomycete fungus and a green algal partner living together in a symbiotic relationship with a cyanobacterium—a symbiosis involving members of three kingdoms of organisms.
 
-dragonhorn: Cladonia squamosa The primary thallus is composed of medium-sized, crenate squamules, approximately 1.5-7mm. long, and 1-5mm. wide. The upper surface is fawn or tan to cinnamon-colored varying toward greenish grey. The apothecia are small, ranging from .5–3 mm. in diameter, and are located on the margin of the cups or at the ends of branches or proliferations. They are fawn to cinnamon-colored. The paraphyses are usually simple, sometimes thickened, and are brownish towards the apex. The hymenium is pale or pale-brownish below and brownish above. The asci are lecanoralean, with a thickened tholus. There are a usually 8 ascospores, which are oblong or oblong-obtuse to fusiform in shape, between 5-17 μm. long and 2.5-3.5 μm. wide. Conidia are falcate and 3-8 μm. long.
+dragonhorn: Cladonia squamosa The primary thallus is composed of medium-sized, crenate squamules, approximately 1.5-7mm long, and 1-5mm wide. The upper surface is fawn or tan to cinnamon-colored varying toward greenish grey. The apothecia are small, ranging from .5–3mm in diameter, and are located on the margin of the cups or at the ends of branches or proliferations. They are fawn to cinnamon-colored. The paraphyses are usually simple, sometimes thickened, and are brownish towards the apex. The hymenium is pale or pale-brownish below and brownish above. The asci are lecanoralean, with a thickened tholus. There are a usually 8 ascospores, which are oblong or oblong-obtuse to fusiform in shape, between 5-17μm long and 2.5-3.5μm wide. Conidia are falcate and 3-8μm long.
 
 reindeer: Cladonia rangiferina, also known as reindeer cup lichen, reindeer lichen (cf. Sw. renlav) or grey reindeer lichen, is a light-coloured fruticose, cup lichen species in the family Cladoniaceae. It grows in both hot and cold climates in well-drained, open environments. Found primarily in areas of alpine tundra, it is extremely cold-hardy. 
 
-cylpow: Cladonia cylindrica Thallus dimorphic, composed of a squamulose primary thallus and fruticose secondary thallus consisting of podetia. Squamules persistent, sorediate or not, dentate to lobed, usually with sorediate margins; ~1.5–3 mm wide by 1.5–3 mm long; upper surface blue-gray, smooth, corticate; lower surface white, ecorticate. Podetia simple, tapering, without cups, ecorticate, sorediate, covered in coarse to fine soredia; ~3–5 mm tall by 0.5–1 mm thick. Ascomata biatorine apothecia, brown, usually inconspicuous; pycnidia brown, usually inconspicuous.
+cylpow: Cladonia cylindrica Thallus dimorphic, composed of a squamulose primary thallus and fruticose secondary thallus consisting of podetia. Squamules persistent, sorediate or not, dentate to lobed, usually with sorediate margins; ~1.5–3mm wide by 1.5–3mm long; upper surface blue-gray, smooth, corticate; lower surface white, ecorticate. Podetia simple, tapering, without cups, ecorticate, sorediate, covered in coarse to fine soredia; ~3–5mm tall by 0.5–1mm thick. Ascomata biatorine apothecia, brown, usually inconspicuous; pycnidia brown, usually inconspicuous.
 
+enada: Amanita bisporigera
 
+cl: Amanita bisporigera is a deadly poisonous species of fungus in the family Amanitaceae. It is commonly known as the eastern destroying angel amanita, the eastern North American destroying angel or just as the destroying angel, although the fungus shares this latter name with three other lethal white Amanita species, A. ocreata, A. verna and A. virosa. The mushroom has a smooth white cap that can reach up to 10cm across and a stipe up to 14cm tall with a white skirt-like ring near the top. The bulbous stipe base is covered with a membranous sac-like volva. The white gills are free from attachment to the stalk and crowded closely together. As the species name suggests, A. bisporigera typically bears two spores on the basidia, although this characteristic is not immutable. A. bisporigera closely resembles a few other white amanitas, including the equally deadly A. virosa and A. verna. 
 
+so: Resupinatus applicatus, commonly known as the smoked oysterling or the black jelly oyster, is a species of fungus in the family Tricholomataceae.
 
+ge: Naematelia aurantia (synonym Tremella aurantia) is a species of fungus producing yellow, frondose, gelatinous basidiocarps (fruit bodies).
 
-enada:
+chrom: Chromosera is a genus of fungi in the family Hygrophoraceae. Within the family Hygrophoraceae it is closely related to the genus Gloioxanthomyces.
 
-cl: 
+ahm: Pogonatum pensylvanicum occurs on moist bare soil, often on streambanks, in wooded area. Sporophytes mature in late summer/fall.
 
-so:
+reddock: Ramularia rubella Infection produces reddish spots on leaves. The red color is caused by the production of rubellin, a photodynamic anthraquinone-derived phytotoxin.
 
-ge:
+dryad: Cerioporus squamosus is a basidiomycete bracket fungus, with common names including dryad's saddle and pheasant's back mushroom.
 
-chrom:
+lb: Ganoderma lucidum, commonly known as the reishi, varnished conk, or ling chih, is a red-colored species of Ganoderma with a limited distribution in Europe and parts of China, where it grows on decaying hardwood trees. Wild populations have been found in the United States in California and Utah but were likely introduced anthropogenically and naturalized.
 
-ahm:
+vpspore: Trichaptum biforme Tough, hairy bracket fungus with violet margins and zones of white, brown, and black; pores whitish violet, breaking into teeth. Grows on stumps and logs of deciduous wood. Year-round. 
 
-reddock:
+hoyster: Panus neostrigosus
 
-dryad:
+vermwaxcap: Hygrocybe miniata, commonly known as the vermilion waxcap or miniature waxy cap, is a small, bright red or red-orange mushroom of the waxcap genus Hygrocybe. It is a cosmopolitan species, that is found worldwide. In Europe, it is found in fields, on sandy heaths, or grassy commons in the autumn. It is found in rainforest and eucalypt forest as well as heathland in Australia. 
 
-lb:
+tenuipes: Cordyceps tenuipes is a medicinal fungus that parasitizes the pupal and larvae of Lepidoptera, and currently, wild and farmed Cordyceps are mainly parasitic on silkworm larvae.
 
-vpspore:
+wcoral: Clavulina cristata, commonly known as the wrinkled coral fungus, white coral fungus or the crested coral fungus, is a white- or light-colored edible coral mushroom present in temperate areas of the Americas and Europe. It is the type species of the genus Clavulina.
 
-hoyster:
+cbf: Yxarium nucleatum is a species of fungus in the family Hyaloriaceae. In the UK, it has been given the recommended English name of crystal brain.
 
-vermwaxcap:
+cparchment: Stereum complicatum Saprobic; crowded on dead twigs, branches, and stumps of deciduous trees; July through January, overwinters.  The upper surface of these fan-shaped or semicircular caps are orange-cinnamon to reddish-brown, zoned, and silky-hairy. Caps often overlap and are laterally fused. 
 
-tenuipes:
+mycena: Mycena overholtsii, commonly known as the snowbank fairy helmet or fuzzy foot, is a species of fungus in the family Mycenaceae. 
 
-wcoral:
+greenwood: Chlorociboria aeruginascens is a saprobic species of mushroom, commonly known as the blue stain, green elfcup or the green wood cup.
 
-cbf;
+yfc: Bisporella citrina, commonly known as yellow fairy cups or lemon discos, is a species of fungus in the family Helotiaceae. The fungus produces tiny yellow cups up to 3mm diameter, often without stalks, that fruit in groups or dense clusters on decaying deciduous wood that has lost its bark.
 
-cparchment:
+panellus: Panellus stipticus, commonly known as the bitter oyster, the astringent panus, the luminescent panellus, or the stiptic fungus, is a species of fungus. It belongs in the family Mycenaceae, and the type species of the genus Panellus. A common and widely distributed species, it is found in Asia, Australia, Europe, and North America, where it grows in groups or dense overlapping clusters on the logs, stumps, and trunks of deciduous trees, especially beech, oak, and birch. During the development of the fruit bodies, the mushrooms start out as tiny white knobs, which, over a period of one to three months, develop into fan- or kidney-shaped caps that measure up to 3cm broad. The caps are orange-yellow to brownish, and attached to the decaying wood by short stubby stalks that are connected off-center or on the side of the caps.
 
-mycena;
+gfmould: Botrytis cinerea is a necrotrophic fungus that affects many plant species, although its most notable hosts may be wine grapes. In viticulture, it is commonly known as "botrytis bunch rot"; in horticulture, it is usually called "grey mould" or "gray mold". 
 
-greenwood:
+mpball: Lycoperdon pratense, commonly known as the meadow puffball, is a type of puffball mushroom in the genus Lycoperdon. It is found in Great Britain and Ireland, including the Outer Hebrides, mainland Europe, and occasionally in North America, and is commonly seen in sand dune systems, where it can be abundant in dune slacks as well as in grassland and lawns, as its vernacular name suggests. In the early stages of development its skin is scruffy whereas its bigger relatives have smooth skins; the meadow puffball is edible and has a stump-like stem (to differentiate it from the giant puffball). 
 
-yfc;
+et: With the name Neolecta irregularis, usually adapted as the common name “irregular earth tongue”, this fungus can be identified in the woods of North America and Japan by its irregular and unbranched yellow fruiting bodies that appear sprouting from the ground near trees.
 
-panellus;
+ttail: Trametes versicolor – also known as Coriolus versicolor and Polyporus versicolor – is a common polypore mushroom found throughout the world. Meaning 'of several colors', versicolor accurately describes this fungus that displays a unique blend of markings. Additionally, owing to its shape being similar to that of the tail feathers of a wild turkey. T. versicolor is most commonly referred to as turkey tail.
 
-gfmould:
-
-mpball:
-
-et:
-
-ttail:
-
-tubelet:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tubelet: Henningsomyces candidus can be distinguished by its growth in groups of hundreds or more on well-decayed conifer logs and their tiny tubular white fruitbodies. A hand lens is needed to appreciate their beauty.
