5ea45a1fffadacc521fd30c1f0f8d1ef987bde4c
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2024-11-28 18:51:59 -0500

    Update and rename myco.js to index.js

---
+++ index.js
@@ -0,0 +1,70 @@
+var $parent = $("#main"),
+	$aside = $("#aside"),
+	$asideTarget = $aside.find(".aside--details"),
+	$asideClose = $aside.find(".close"),
+	$tilesParent = $(".tiles-a"),
+	$tiles = $tilesParent.find("a"),
+	slideClass = "show-detail";
+	$tiles.on("click", function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		if(!$("html").hasClass(slideClass)){
+			$tiles.removeClass("active");
+			$(this).addClass("active");
+			$(this).attr("aria-expanded","true");
+			loadTileData($(this));
+			}else{
+				killAside();
+				$(this).attr("aria-expanded","false");
+				}
+			});
+			
+		$asideClose.on("click", function(e){
+			e.preventDefault();
+			killAside();
+		});
+		
+		function loadTileData(target){
+			var $this = $(target),
+			itemHtml = $this.find(".details").html();
+			$asideTarget.html(itemHtml);
+			showAside();
+		}
+		
+		function showAside(){
+			if(!$("html").hasClass(slideClass)){
+				$("html").toggleClass(slideClass);
+				$aside.attr("aria-hidden","false");
+				focusCloseButton();
+			}
+		}
+		
+		window.addEventListener("keyup", function(e){
+			var code = (e.keyCode ? e.keyCode : e.which);
+			if(code === 27){
+				killAside();
+			}
+		}, false);
+		
+		function killAside(){
+			if($("html").hasClass(slideClass)){
+				$("html").removeClass(slideClass);
+				sendFocusBack();
+				$aside.attr("aria-hidden","true");
+				$tiles.attr("aria-expanded","false");
+			}
+		}
+		
+		function focusCloseButton(){
+			$asideClose.focus();
+		}
+		
+		function sendFocusBack(){
+			$(".active").focus();
+		}
+		
+		$parent.on("click",function(e){
+			if($("html").hasClass(slideClass)){
+				killAside();
+			}
+		});
